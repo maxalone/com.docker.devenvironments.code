91fa8357d5629fb9068c3746eea5c304c34e8544
maxalone <mascam64@gmail.com>
2022-01-17 14:08:22 +0000

    mypy primo commit

---
+++ mypy1.py
@@ -0,0 +1,20 @@
+def miafunzione(nome):
+    if nome=="max":
+        print("bentornato " + nome)
+        x=0
+        while x<len(nome):
+            if x==len(nome)-1:
+                print(nome[x], end="")
+                break
+            else:   
+                print(nome[x]+"-", end="")
+                x+=1
+        
+    else:
+        print("piacere di conoscerti " + nome)
+
+print ("---- py from idle native windows -------")
+
+nomee = input("il tuo nome? ")
+
+miafunzione(nomee)
